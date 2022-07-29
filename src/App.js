2167702ec7e247e7646db6e6f1c5a134491c018b
Andrew Smith <topcodeandrew@gmail.com>
2022-07-29 15:09:03 -0600

    removed all redux

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import "./App.css";
 import Header from "./Components/Header";
 import MainDisplay from "./Components/MainDisplay";
 import OptionDisplay from "./Components/OptionDisplay";
-import { selectDisplay } from "./redux/slices/displayCountrySlice";
 
 function App() {
-    let currentDisplay = useSelector(selectDisplay);
     return (
         <div className="App font-link">
             <Header />
-            {currentDisplay ? <MainDisplay /> : <OptionDisplay />}
+            <OptionDisplay />
         </div>
     );
 }
