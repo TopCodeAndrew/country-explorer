098713f7f06aa7890c14a2bb75d8e1e9fc775d8d
Andrew Smith <topcodeandrew@gmail.com>
2022-07-29 13:52:16 -0600

    removed OptionDisplay contents AND 3 child components of MainDisplay.js

--- src/Components/MainDisplay.js
+++ src/Components/MainDisplay.js
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { selectDisplay } from "../redux/slices/displayCountrySlice";
-import Overview from "./Overview";
-import Symbols from "./Symbols";
-import Weather from "./Weather";
 
 const MainDisplay = () => {
     const [view, setView] = useState("Overview");
@@ -17,9 +14,6 @@ const MainDisplay = () => {
                 </button>
                 <button onClick={() => setView("Symbols")}>Symbols</button>
             </div>
-            {view === "Overview" && <Overview />}
-            {view === "Weather" && <Weather />}
-            {view === "Symbols" && <Symbols />}
         </div>
     );
 };
--- src/Components/OptionDisplay.js
+++ src/Components/OptionDisplay.js
@@ -1,33 +1,7 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { setDisplayCountry } from "../redux/slices/displayCountrySlice";
-
-import { selectPotentials } from "../redux/slices/potentialCountriesSlice";
 
 const OptionDisplay = () => {
-    let currentPotentials = useSelector(selectPotentials);
-    const dispatch = useDispatch();
-
-    return (
-        <div className="stack">
-            {currentPotentials &&
-                currentPotentials.map((e, i) => {
-                    return (
-                        <h2
-                            key={e.name.official}
-                            className="country-option"
-                            onClick={() =>
-                                dispatch(
-                                    setDisplayCountry(currentPotentials[i])
-                                )
-                            }
-                        >
-                            {e.name.common}
-                        </h2>
-                    );
-                })}
-        </div>
-    );
+    return <div className="stack">OptionDisplay</div>;
 };
 
 export default OptionDisplay;
