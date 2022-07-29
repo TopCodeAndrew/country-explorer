6d8dff5f9ae2a8f73f1350c4100397fdea8e42a3
Andrew Smith <topcodeandrew@gmail.com>
2022-07-29 11:08:42 -0600

    removed Header from App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useSelector } from "react-redux";
 import "./App.css";
-import Header from "./Components/Header";
 import LoadingModal from "./Components/LoadingModal";
 import MainDisplay from "./Components/MainDisplay";
 import OptionDisplay from "./Components/OptionDisplay";
@@ -12,9 +11,8 @@ function App() {
     let isLoading = useSelector(selectLoading);
     return (
         <div className="App font-link">
-            Starter Code
             {isLoading && <LoadingModal />}
-            <Header />
+
             {currentDisplay ? <MainDisplay /> : <OptionDisplay />}
         </div>
     );
